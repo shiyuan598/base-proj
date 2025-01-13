01a2673b817c28ae4ebf83d061c2cca2af44f562
wangshiyuan <wangshiyuan@zhito.com>
2025-01-13 10:04:58 +0800

    init

---
+++ src/main/java/com/base/BaseProjApplication.java
@@ -0,0 +1,14 @@
+package com.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseProjApplication.class, args);
+        System.out.println("hello world");
+    }
+
+}
