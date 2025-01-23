c537e7ffc7d33288acb69f6957e2b1273d5c84f3
wangshiyuan <wangshiyuan@zhito.com>
2025-01-23 14:05:19 +0800

    增加VO的schema描述

--- src/main/java/com/base/vm/entity/vo/VehicleDictVO.java
+++ src/main/java/com/base/vm/entity/vo/VehicleDictVO.java
@@ -1,13 +1,21 @@
 package com.base.vm.entity.vo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "车辆字典")
 public class VehicleDictVO {
+    @Schema(description = "车辆ID")
     private Integer id;
+    @Schema(description = "车辆编号")
     private String vehicleNo;
+    @Schema(description = "项目名称")
     private String project;
+    @Schema(description = "车辆状态")
     private Integer state;
+    @Schema(description = "车辆状态名称")
     private String stateName;
+    @Schema(description = "车辆地点")
     private String place;
 }
--- src/main/java/com/base/vm/entity/vo/VehicleListVO.java
+++ src/main/java/com/base/vm/entity/vo/VehicleListVO.java
@@ -1,11 +1,17 @@
 package com.base.vm.entity.vo;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "车辆列表")
 public class VehicleListVO {
+    @Schema(description = "车辆ID")
     private Integer id;
+    @Schema(description = "项目名称")
     private String project;
+    @Schema(description = "车辆编号")
     private String vehicleNo;
+    @Schema(description = "车辆状态")
     private Integer state;
 }
