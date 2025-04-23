febbcaf8f279c5988357f132edce70c6efb319c5
wangshiyuan <wangshiyuan@zhito.com>
2025-04-23 16:18:19 +0800

    update swagger url

--- src/main/java/com/base/BaseProjApplication.java
+++ src/main/java/com/base/BaseProjApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.env.Environment;
 
@@ -25,7 +24,7 @@ public class BaseProjApplication implements ApplicationListener<ContextRefreshed
             System.out.println("\n----------------------------------------------------------");
             System.out.println("API文档地址：");
             System.out.println("Knife4j文档：http://localhost:" + serverPort + contextPath + "/doc.html");
-            System.out.println("Swagger文档：http://localhost:" + serverPort + contextPath + "/swagger-ui/listVehicle.html");
+            System.out.println("Swagger文档：http://localhost:" + serverPort + contextPath + "/swagger-ui/index.html");
             System.out.println("----------------------------------------------------------\n");
         }
     }
